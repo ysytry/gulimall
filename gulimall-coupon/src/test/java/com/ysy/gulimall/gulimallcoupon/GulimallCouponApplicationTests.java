a0e790fc579d3ad66dca3de8ce70806e7f9da162
ysyhitry <511858218@qq.com>
2020-06-18 21:15:04 +0800

    项目结构初始化

---
+++ gulimall-coupon/src/test/java/com/ysy/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
